79248a62304170c84fc48777c2284b5cda383563
ethanmmay <ethaniel.may@gmail.com>
2021-02-12 17:05:37 -0700

    JavaScript 98%

--- app.js
+++ app.js
@@ -3,7 +3,12 @@
 let cash = 0
 let programs = 0
 let programValue = 25
-let passivePrograms = 0.0
+let passivePrograms = 0
+let botTemplate = ""
+let botsAmount = 0
+let botImages = true
+let backgroundGIF = true
+setInterval(collectAutoUpgrades, 1000)
 
 let botOne = {
     price: 250,
@@ -12,15 +17,15 @@ let botOne = {
 }
 
 let botTwo = {
-    price: 750,
+    price: 400,
     quantity: 0,
-    passiveProgram: 4
+    passiveProgram: 2
 }
 
 let botThree = {
-    price: 3000,
+    price: 550,
     quantity: 0,
-    passiveProgram: 15
+    passiveProgram: 3
 }
 
 let hackOne = {
@@ -48,20 +53,21 @@ let hackUpgrades = [hackOne, hackTwo, hackThree]
 function makeProgram() {
     programs++
     cash += programValue
-
     update()
 }
 
 function update() {
     document.getElementById("programsArea").innerText = "x" + programs
-    document.getElementById("cashAmount").innerText = "$" + cash
-    document.getElementById("programValueArea").innerText = "$" + programValue
+    document.getElementById("cashAmount").innerText = "$" + Math.round(cash)
+    document.getElementById("programValueArea").innerText = "$" + Math.round(programValue)
     document.getElementById("ppsArea").innerText = "" + passivePrograms
+    document.getElementById("botsAmountArea").innerText = "x" + botsAmount
     for (let i = 0; i < 3; i++) {
-        document.getElementById("botPrice" + i).innerText = "$" + botUpgrades[i].price
+        document.getElementById("botPrice" + i).innerText = "$" + Math.round(botUpgrades[i].price)
         document.getElementById("botQuan" + i).innerText = "x" + botUpgrades[i].quantity
-        document.getElementById("hackPrice" + i).innerText = "$" + hackUpgrades[i].price
+        document.getElementById("hackPrice" + i).innerText = "$" + Math.round(hackUpgrades[i].price)
         document.getElementById("hackQuan" + i).innerText = "x" + hackUpgrades[i].quantity
+        checkAffordable(i)
     }
 }
 
@@ -72,6 +78,13 @@ function addBot(botNum) {
         currentBot.price *= 1.5
         passivePrograms += currentBot.passiveProgram
         currentBot.quantity++
+        for (let i = 0; i < botUpgrades[botNum].passiveProgram; i++) {
+            botsAmount++
+            botTemplate += `<img src="https://robohash.org/${Math.round(Math.random()*9999)}?set=set3" alt="Bot" width="150px" class="bot-img">`
+        }
+        if (botImages) {
+            document.getElementById("botArea").innerHTML = botTemplate
+        }
         update()
     } else {
         alert("You cannot afford this upgrade.")
@@ -90,3 +103,62 @@ function addHack(hackNum) {
         alert("You cannot afford this upgrade.")
     }
 }
+
+
+function collectAutoUpgrades() {
+    if (passivePrograms > 0) {
+        programs += passivePrograms
+        cash += programValue * passivePrograms
+        update()
+    }
+}
+
+function toggleBotImages() {
+    if (botImages) {
+        botImages = false
+        document.getElementById("botImageButton").innerText = "Turn on New Bot Images"
+        document.getElementById("botImageButton").classList.remove("btn-hacker")
+        document.getElementById("botImageButton").classList.add("btn-hacker-off")
+    } else {
+        botImages = true
+        document.getElementById("botImageButton").innerText = "Turn off New Bot Images"
+        document.getElementById("botImageButton").classList.remove("btn-hacker-off")
+        document.getElementById("botImageButton").classList.add("btn-hacker")
+    }
+}
+
+function toggleBackground() {
+    if (backgroundGIF) {
+        backgroundGIF = false
+        document.getElementById("bgGIFButton").innerText = "Turn on Background GIF"
+        document.getElementById("bodyTag").classList.remove("background-GIF")
+        document.getElementById("bgGIFButton").classList.remove("btn-hacker")
+        document.getElementById("bgGIFButton").classList.add("btn-hacker-off")
+    } else {
+        backgroundGIF = true
+        document.getElementById("bgGIFButton").innerText = "Turn off Background GIF"
+        document.getElementById("bodyTag").classList.add("background-GIF")
+        document.getElementById("bgGIFButton").classList.remove("btn-hacker-off")
+        document.getElementById("bgGIFButton").classList.add("btn-hacker")
+    }
+}
+
+function checkAffordable(upgradeIndex) {
+    if (cash < botUpgrades[upgradeIndex].price) {
+        document.getElementById("botPriceButton" + upgradeIndex).classList.remove("btn-hacker")
+        document.getElementById("botPriceButton" + upgradeIndex).classList.add("btn-hacker-off")
+    } else {
+        document.getElementById("botPriceButton" + upgradeIndex).classList.remove("btn-hacker-off")
+        document.getElementById("botPriceButton" + upgradeIndex).classList.add("btn-hacker")
+    }
+    if (cash < hackUpgrades[upgradeIndex].price) {
+        document.getElementById("hackPriceButton" + upgradeIndex).classList.remove("btn-hacker")
+        document.getElementById("hackPriceButton" + upgradeIndex).classList.add("btn-hacker-off")
+    } else {
+        document.getElementById("hackPriceButton" + upgradeIndex).classList.remove("btn-hacker-off")
+        document.getElementById("hackPriceButton" + upgradeIndex).classList.add("btn-hacker")
+    }
+    
+}
+
+update()
